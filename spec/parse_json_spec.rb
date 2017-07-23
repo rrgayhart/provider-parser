dfe321fecfc0534ed18dcc090d8cf3da814298ac
rrgayhart <rrgayhart@gmail.com>
2017-07-23 11:20:46 -0600

    Clarify assertion on parse_json spec

--- spec/parse_json_spec.rb
+++ spec/parse_json_spec.rb
@@ -5,8 +5,8 @@ describe 'parse_json' do
     records = parse_json('spec/data/long_source_data.json')
     expect(records.length).to eq(11231)
     all_valid_hash_data = records.all?{ |r| r.class == Hash }
-    all_valid_keys = records.all?{ |r| r.keys.sort == ["doctor", "practices"] }
-    expect(all_valid_keys).to be(true)
+    all_valid_top_level_keys = records.all?{ |r| r.keys.sort == ['doctor', 'practices'] }
+    expect(all_valid_top_level_keys).to be(true)
     expect(all_valid_hash_data).to be(true)
   end
 end
